0a546848583a5eb244cd149c4cf4d2ee17f8f30b
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-04-26 13:35:07 +0300

    Добавьте файлы проекта.

---
+++ stack template/stack template.cpp
@@ -0,0 +1,23 @@
+﻿//Доопрацюйте клас Stack. Зpобіть так, щоб клас Stack був шаблонним. 
+#include <iostream>
+#include "Stack.h"
+
+using namespace std;
+int main()
+{
+	setlocale(0, "Ukrainian");
+	Stack<int>A(3);
+
+	cout <<   A.push(45)<<endl;
+	cout <<   A.push(55)<<endl;
+	cout << A.push(95)<<endl;
+	cout << "стек повний :" << A.isFull() << endl;
+
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << A.pop() << "\t";
+	}
+	cout << endl;
+	cout << "стек пустий :" << A.isEmpty()<<endl;
+	
+}
